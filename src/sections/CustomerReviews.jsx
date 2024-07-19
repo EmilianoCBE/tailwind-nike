5481164c3444c539617eaf94b649b1ddf6236884
EmilianoCBE <e.carrizales1305@gmail.com>
2024-07-19 14:13:29 -0600

    Max-container class added to Reviews section

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -3,7 +3,7 @@ import { reviews } from '../constants'
 
 export const CustomerReviews = () => {
   return (
-    <section className="">
+    <section className="max-container">
       <h3 className="font-palanquin text-center text-4xl font-bold">
         What Our
         <span className="text-coral-red"> Customers </span>
